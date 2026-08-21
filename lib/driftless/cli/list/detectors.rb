7294d4dfafb208708a475f1a4c6d6b1c6f882153
Chris Tessmer <chris.tessmer@onyxpoint.com>
2026-08-21 17:38:16 +0000

    Colorize `driftless list detectors` by default

--- lib/driftless/cli/list/detectors.rb
+++ lib/driftless/cli/list/detectors.rb
@@ -1,6 +1,7 @@
 require 'driftless/cli/base'
 require 'driftless/cli/list'
 require 'driftless/detectors'
+require 'driftless/ansi'
 
 module Driftless
   module CLI
@@ -9,11 +10,19 @@ module Driftless
         register_command name: 'detectors', subcommand_of: List
         desc 'List all detector keys and their descriptions'
 
+        include Ansi
+
         def execute(_argv)
           registry = ::Driftless::Detectors.registry.sort_by(&:key)
           width = registry.map { |k| k.key.size }.max
           registry.each do |klass|
-            puts "#{klass.key.ljust(width)}   #{klass.about}"
+            title_column = klass.key.ljust(width)
+            about_column = klass.about
+            if @options.fetch(:color,true)
+              title_column = Ansi.wrap(title_column, :bold)
+              about_column = Ansi.wrap(about_column, :cyan)
+            end
+            puts "#{title_column}   #{about_column}"
           end
           exit 0
         end
